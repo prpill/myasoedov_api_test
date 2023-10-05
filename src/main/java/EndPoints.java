7e369fbfba0d7e513674c522855bcc02692b5330
Myasoedov Ivan <i.myasoedov@sh.dunice.net>
2023-10-05 17:40:49 +0300

    Test basis

---
+++ src/main/java/EndPoints.java
@@ -0,0 +1,8 @@
+public class EndPoints {
+    private final String BASE_URL = "https://api.news.academy.dunice.net";
+    public final String SIGN_UP = "/auth/signup";
+    public final String LOG_IN = "/auth/login";
+    public final String USER_ACTION = "/users/";
+    public final String POST_ACTION = "/posts/";
+    public final String COMMENT_ACTION = "/comments/";
+}
